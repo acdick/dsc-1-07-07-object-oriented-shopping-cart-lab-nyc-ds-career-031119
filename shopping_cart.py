5eca18ceaba1b8f3631b860d6895eda67fb39c14
Adam Dick <adam.c.dick@gmail.com>
2019-03-15 15:54:56 -0400

    updated add_item

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,12 +1,19 @@
 class ShoppingCart:
     # write your code here
     def __init__(self, emp_discount=None):
-      self.total = 0
-      self.employee_discount = emp_discount
-      self.items = []
+        self.total = 0
+        self.employee_discount = emp_discount
+        self.items = []
 
     def add_item(self, name, price, quantity=1):
-       pass
+        item = {}
+        item['name'] = name
+        item['price'] = price
+        item['quantity'] = quantity
+        items.append(item)
+        self.total += price * quantity
+        return self.total
+
     def mean_item_price(self):
        pass
 
